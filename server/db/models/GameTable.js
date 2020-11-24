0acf92224ef5f358da14981a3abab157eccbf56d
John Cook <jpcook72@gmail.com>
2020-11-24 14:49:04 -0600

    Adding some endlines

--- server/db/models/GameTable.js
+++ server/db/models/GameTable.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const GameTable = db.define('gameTable', {
-    accessCode: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-  })
-  
-  module.exports = GameTable
+accessCode: {
+    type: Sequelize.STRING,
+    allowNull: false
+}
+})
+
+module.exports = GameTable
