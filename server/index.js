6af599c80cfdbfd0b4c6ccbfaabb6086ac12cb33
qinben10 <qinben10@gmail.com>
2020-11-26 14:13:14 -0500

    build server and front end for webRTC

--- server/index.js
+++ server/index.js
@@ -21,14 +21,57 @@ app.use((err, req, res, next) => {
     res.status(500).send('Error:' + err.message);
   });
 
-const init = () => {
-try {
-    const port = process.env.PORT || 8080;
-    app.listen(port, () => console.log(`listening on port ${port}`));
-}
-catch (ex){
-    console.log(ex);
-}
-};
 
-init();
+
+  ///webRTC PART:
+
+  const ws = require('ws');
+  
+  const port = process.env.PORT || 8080;
+  const server = app.listen(port, () => console.log(`listening on port ${port}`));
+  
+  const wsServer = new ws.Server({
+    server,
+  });
+  
+  const peersByCode = {};
+  
+  wsServer.on('request', request => {
+    const connection = request.accept();
+    const id = (Math.random() * 10000);
+    clients.push({ connection, id });
+  
+    connection.on('message', message => {
+      console.log(message);
+      clients
+        .filter(client => client.id !== id)
+        .forEach(client => client.connection.send(message.utf8Data));
+    });
+  
+    connection.on('close', () => {
+      clients = clients.filter(client => client.id !== id);
+    });
+  });
+
+
+
+
+
+
+
+
+  /////
+
+// const init = () => {
+// try {
+//     const port = process.env.PORT || 8080;
+//     app.listen(port, () => console.log(`listening on port ${port}`));
+// }
+// catch (ex){
+//     console.log(ex);
+// }
+// };
+
+
+
+// init();
