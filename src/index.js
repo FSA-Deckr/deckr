9566a9c47d978cc10fed7789edf7da43fb929c2a
qinben10 <qinben10@gmail.com>
2020-11-26 17:48:53 -0500

    webRTC v2.1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser;
   
 //connecting to our signaling server
-var conn = new WebSocket('ws://localhost:8080');
+var conn = new WebSocket(window.location.origin.replace('http', 'ws'));
   
 conn.onopen = function () { 
    console.log("Connected to the signaling server"); 
