b374b6308a9568e96644ce977a3eb4f476a550b8
qinben10 <qinben10@gmail.com>
2020-11-26 17:27:51 -0500

    webRTC v2

--- src/index.js
+++ src/index.js
@@ -1,105 +1,209 @@
-(function () {
-  "use strict";
+//our username 
+var name; 
+var connectedUser;
+  
+//connecting to our signaling server
+var conn = new WebSocket('ws://localhost:8080');
+  
+conn.onopen = function () { 
+   console.log("Connected to the signaling server"); 
+};
+  
+//when we got a message from a signaling server 
+conn.onmessage = function (msg) { 
+   console.log("Got message", msg.data);
+	
+   var data = JSON.parse(msg.data); 
+	
+   switch(data.type) { 
+      case "login": 
+         handleLogin(data.success); 
+         break; 
+      //when somebody wants to call us 
+      case "offer": 
+         handleOffer(data.offer, data.name); 
+         break; 
+      case "answer": 
+         handleAnswer(data.answer); 
+         break; 
+      //when a remote peer sends an ice candidate to us 
+      case "candidate": 
+         handleCandidate(data.candidate); 
+         break; 
+      case "leave": 
+         handleLeave(); 
+         break; 
+      default: 
+         break; 
+   }
+};
+  
+conn.onerror = function (err) { 
+   console.log("Got error", err); 
+};
+  
+//alias for sending JSON encoded messages 
+function send(message) { 
+   //attach the other peer username to our messages 
+   if (connectedUser) { 
+      message.name = connectedUser; 
+   } 
+	
+   conn.send(JSON.stringify(message)); 
+};
+  
+//****** 
+//UI selectors block 
+//******
+ 
+var loginPage = document.querySelector('#loginPage'); 
+var usernameInput = document.querySelector('#usernameInput'); 
+var loginBtn = document.querySelector('#loginBtn'); 
 
-  const MESSAGE_TYPE = {
-    SDP: 'SDP',
-    CANDIDATE: 'CANDIDATE',
-  }
+var callPage = document.querySelector('#callPage'); 
+var callToUsernameInput = document.querySelector('#callToUsernameInput');
+var callBtn = document.querySelector('#callBtn'); 
 
-  document.addEventListener('click', async (event) => {
-    if (event.target.id === 'start') {
-      startChat();
-    }
-  });
+var hangUpBtn = document.querySelector('#hangUpBtn');
+  
+var localVideo = document.querySelector('#localVideo'); 
+var remoteVideo = document.querySelector('#remoteVideo'); 
 
-  const startChat = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-      showChatRoom();
+var yourConn; 
+var stream;
+  
+callPage.style.display = "none";
 
-      const signaling = new WebSocket('ws://127.0.0.1:8080');
-      const peerConnection = createPeerConnection(signaling);
+// Login when the user clicks the button 
+loginBtn.addEventListener("click", function (event) { 
+   name = usernameInput.value;
+	
+   if (name.length > 0) { 
+      send({ 
+         type: "login", 
+         name: name 
+      }); 
+   }
+	
+});
+  
+function handleLogin(success) { 
+   if (success === false) { 
+      alert("Ooops...try a different username"); 
+   } else { 
+      loginPage.style.display = "none"; 
+      callPage.style.display = "block";
+		
+      //********************** 
+      //Starting a peer connection 
+      //********************** 
+		
+      //getting local video stream 
+      navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) { 
+         stream = myStream; 
+			
+         //displaying local video stream on the page 
+         localVideo.srcObject = stream;
+			
+         //using Google public stun server 
+         var configuration = { 
+            "iceServers": [{ "url": "stun:stun.l.test.com:19000" }]
+         }; 
+			
+         yourConn = new webkitRTCPeerConnection(configuration); 
+			
+         // setup stream listening 
+         yourConn.addStream(stream); 
+			
+         //when a remote user adds stream to the peer connection, we display it 
+         yourConn.onaddstream = function (e) { 
+            remoteVideo.srcObject = e.stream; 
+         };
+			
+         // Setup ice handling 
+         yourConn.onicecandidate = function (event) { 
+            if (event.candidate) { 
+               send({ 
+                  type: "candidate", 
+                  candidate: event.candidate 
+               }); 
+            } 
+         };  
+			
+      }, function (error) { 
+         console.log(error); 
+      }); 
+		
+   } 
+};
+  
+//initiating a call 
+callBtn.addEventListener("click", function () { 
+   var callToUsername = callToUsernameInput.value;
+	
+   if (callToUsername.length > 0) { 
+	
+      connectedUser = callToUsername;
+		
+      // create an offer 
+      yourConn.createOffer(function (offer) { 
+         send({ 
+            type: "offer", 
+            offer: offer 
+         }); 
+			
+         yourConn.setLocalDescription(offer); 
+      }, function (error) { 
+         alert("Error when creating an offer"); 
+      });
+		
+   } 
+});
+  
+//when somebody sends us an offer 
+function handleOffer(offer, name) { 
+   connectedUser = name; 
+   yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+	
+   //create an answer to an offer 
+   yourConn.createAnswer(function (answer) { 
+      yourConn.setLocalDescription(answer); 
+		
+      send({ 
+         type: "answer", 
+         answer: answer 
+      }); 
+		
+   }, function (error) { 
+      alert("Error when creating an answer"); 
+   }); 
+};
+  
+//when we got an answer from a remote user
+function handleAnswer(answer) { 
+   yourConn.setRemoteDescription(new RTCSessionDescription(answer)); 
+};
+  
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) { 
+   yourConn.addIceCandidate(new RTCIceCandidate(candidate)); 
+};
+   
+//hang up 
+hangUpBtn.addEventListener("click", function () { 
 
-      addMessageHandler(signaling, peerConnection);
-
-      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-      document.getElementById('self-view').srcObject = stream;
-
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const createPeerConnection = (signaling) => {
-    const peerConnection = new RTCPeerConnection({
-      iceServers: [{ urls: 'stun:stun.l.test.com:19000' }],
-    });
-    
-    peerConnection.onnegotiationneeded = async () => {
-     await createAndSendOffer(signaling, peerConnection);
-    };
-
-    peerConnection.onicecandidate = (iceEvent) => {
-      if (iceEvent && iceEvent.candidate) {
-        signaling.send(JSON.stringify({
-          message_type: MESSAGE_TYPE.CANDIDATE,
-          content: iceEvent.candidate,
-        }));
-      }
-    };
-
-    peerConnection.ontrack = (event) => {
-      const video = document.getElementById('remote-view');
-      if (!video.srcObject) {
-        video.srcObject = event.streams[0];
-      }
-    };
-
-    return peerConnection;
-  };
-
-  const addMessageHandler = (signaling, peerConnection) => {
-    signaling.onmessage = async (message) => {
-      const data = JSON.parse(message.data);
-
-      if (!data) {
-        return;
-      }
-
-      const { message_type, content } = data;
-      try {
-        if (message_type === MESSAGE_TYPE.CANDIDATE && content) {
-          await peerConnection.addIceCandidate(content);
-        } else if (message_type === MESSAGE_TYPE.SDP) {
-          if (content.type === 'offer') {
-            await peerConnection.setRemoteDescription(content);
-            const answer = await peerConnection.createAnswer();
-            await peerConnection.setLocalDescription(answer);
-            signaling.send(JSON.stringify({
-              message_type: MESSAGE_TYPE.SDP,
-              content: answer,
-            }));
-          } else if (content.type === 'answer') {
-            await peerConnection.setRemoteDescription(content);
-          } else {
-            console.log('Unsupported SDP type.');
-          }
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    };
-  };
-
-  const createAndSendOffer = async (signaling, peerConnection) => {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    signaling.send(JSON.stringify({ message_type: MESSAGE_TYPE.SDP, content: offer }));
-  };
-
-  const showChatRoom = () => {
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('chat-room').style.display = 'block';
-  };
-})();
+   send({ 
+      type: "leave" 
+   });  
+	
+   handleLeave(); 
+});
+  
+function handleLeave() { 
+   connectedUser = null; 
+   remoteVideo.src = null; 
+	
+   yourConn.close(); 
+   yourConn.onicecandidate = null; 
+   yourConn.onaddstream = null; 
+};
