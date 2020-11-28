6de89a25ed897485abfb3af4930ff27dfdc87d98
qinben10 <qinben10@gmail.com>
2020-11-28 13:01:12 -0500

    agora_v1

--- src/index.js
+++ src/index.js
@@ -1,201 +1,77 @@
-//our username 
-var name; 
-var connectedUser;
-  
-//connecting to our signaling server
-var conn = new WebSocket(window.location.origin.replace('http', 'ws'));
-  
-conn.onopen = function () { 
-   console.log("Connected to the signaling server"); 
-};
-  
-//when we got a message from a signaling server 
-conn.onmessage = function (msg) { 
-   console.log("Got message", msg.data);
-	
-   var data = JSON.parse(msg.data); 
-	
-   switch(data.type) { 
-      case "login": 
-         handleLogin(data.success); 
-         break; 
-      //when somebody wants to call us 
-      case "offer": 
-         handleOffer(data.offer, data.name); 
-         break; 
-      case "answer": 
-         handleAnswer(data.answer); 
-         break; 
-      //when a remote peer sends an ice candidate to us 
-      case "candidate": 
-         handleCandidate(data.candidate); 
-         break; 
-      case "leave": 
-         handleLeave(); 
-         break; 
-      default: 
-         break; 
-   }
-};
-  
-conn.onerror = function (err) { 
-   console.log("Got error", err); 
-};
-  
-//alias for sending JSON encoded messages 
-function send(message) { 
-   //attach the other peer username to our messages 
-   if (connectedUser) { 
-      message.name = connectedUser; 
-   } 
-	
-   conn.send(JSON.stringify(message)); 
-};
-  
-//****** 
-//UI selectors block 
-//******
- 
-var loginPage = document.querySelector('#loginPage'); 
-var usernameInput = document.querySelector('#usernameInput'); 
-var loginBtn = document.querySelector('#loginBtn'); 
+import AgoraRTC from 'agora-rtc-sdk'
 
-var callPage = document.querySelector('#callPage'); 
-var callToUsernameInput = document.querySelector('#callToUsernameInput');
-var callBtn = document.querySelector('#callBtn'); 
+let handleError = function(err){
+    console.log("Error: ", err);
+};
 
-var hangUpBtn = document.querySelector('#hangUpBtn');
-  
-var localVideo = document.querySelector('#localVideo'); 
-var remoteVideo = document.querySelector('#remoteVideo'); 
+// Query the container to which the remote stream belong.
+let remoteContainer = document.getElementById("remote-container");
 
-var yourConn; 
-var stream;
-  
-callPage.style.display = "none";
+// Add video streams to the container.
+function addVideoStream(elementId){
+    // Creates a new div for every stream
+    let streamDiv = document.createElement("div");
+    // Assigns the elementId to the div.
+    streamDiv.id = elementId;
+    // Takes care of the lateral inversion
+    streamDiv.style.transform = "rotateY(180deg)";
+    // Adds the div to the container.
+    remoteContainer.appendChild(streamDiv);
+};
 
-// Login when the user clicks the button 
-loginBtn.addEventListener("click", function (event) { 
-   name = usernameInput.value;
-	
-   if (name.length > 0) { 
-      send({ 
-         type: "login", 
-         name: name 
-      }); 
-   }
-	
+// Remove the video stream from the container.
+function removeVideoStream(elementId) {
+    let remoteDiv = document.getElementById(elementId);
+    if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+};
+
+
+let client = AgoraRTC.createClient({
+    mode: "rtc",
+    codec: "vp8",
 });
-  
-async function handleLogin(success) { 
-   if (success === false) { 
-      alert("Ooops...try a different username"); 
-   } else { 
-      loginPage.style.display = "none"; 
-      callPage.style.display = "block";
-		
-      //********************** 
-      //Starting a peer connection 
-      //********************** 
-		
-      //getting local video stream 
 
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      localVideo.srcObject = stream;
+client.init("bb8fa8a838674e38abd095b569e17cfc");
 
-      const configuration = { 
-         "iceServers": [{ "url": "stun:stun.l.google.com:19302" }]
-      }; 
-      yourConn = new RTCPeerConnection(configuration);
-      console.log(yourConn)
-      // setup stream listening 
-      yourConn.addStream(stream); 
-      
-      //when a remote user adds stream to the peer connection, we display it 
-      yourConn.onaddstream = function (e) { 
-         remoteVideo.srcObject = e.stream; 
-      };
-      
-      // Setup ice handling 
-      yourConn.onicecandidate = function (event) { 
-         if (event.candidate) { 
-            send({ 
-               type: "candidate", 
-               candidate: event.candidate 
-            }); 
-         } 
-      }; 
-   } 
-};
-  
-//initiating a call 
-callBtn.addEventListener("click", function () { 
-   var callToUsername = callToUsernameInput.value;
-	
-   if (callToUsername.length > 0) { 
-	
-      connectedUser = callToUsername;
-		
-      // create an offer 
-      yourConn.createOffer(function (offer) { 
-         send({ 
-            type: "offer", 
-            offer: offer 
-         }); 
-			
-         yourConn.setLocalDescription(offer); 
-      }, function (error) { 
-         alert("Error when creating an offer"); 
-      });
-		
-   } 
+// Join a channel
+client.join("006bb8fa8a838674e38abd095b569e17cfcIADn/1CncSVzxOlkEGskHXTi5gY5RPCcZbooZ9kyDItBdaT2+9QAAAAAEAAWal0mYeHDXwEAAQBh4cNf", "deckr", null, (uid)=>{
+    let localStream = AgoraRTC.createStream({
+        audio: true,
+        video: true,
+    });
+    // Initialize the local stream
+    localStream.init(()=>{
+        // Play the local stream
+        localStream.play("me");
+        // Publish the local stream
+        client.publish(localStream, handleError);
+    }, handleError);
+  }, handleError);
+
+
+// Subscribe to the remote stream when it is published
+client.on("stream-added", function(evt){
+    client.subscribe(evt.stream, handleError);
 });
-  
-//when somebody sends us an offer 
-function handleOffer(offer, name) { 
-   connectedUser = name; 
-   yourConn.setRemoteDescription(new RTCSessionDescription(offer));
-	
-   //create an answer to an offer 
-   yourConn.createAnswer(function (answer) { 
-      yourConn.setLocalDescription(answer); 
-		
-      send({ 
-         type: "answer", 
-         answer: answer 
-      }); 
-		
-   }, function (error) { 
-      console.log(error)
-      alert("Error when creating an answer"); 
-   }); 
-};
-  
-//when we got an answer from a remote user
-function handleAnswer(answer) { 
-   yourConn.setRemoteDescription(new RTCSessionDescription(answer)); 
-};
-  
-//when we got an ice candidate from a remote user 
-function handleCandidate(candidate) { 
-   yourConn.addIceCandidate(new RTCIceCandidate(candidate)); 
-};
-   
-//hang up 
-hangUpBtn.addEventListener("click", function () { 
-
-   send({ 
-      type: "leave" 
-   });  
-	
-   handleLeave(); 
+// Play the remote stream when it is subsribed
+client.on("stream-subscribed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
 });
-  
-function handleLeave() { 
-   connectedUser = null; 
-   remoteVideo.src = null; 
-	
-   yourConn.close(); 
-   yourConn.onicecandidate = null; 
-   yourConn.onaddstream = null; 
-};
+
+// Remove the corresponding view when a remote user unpublishes.
+client.on("stream-removed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+});
+// Remove the corresponding view when a remote user leaves the channel.
+client.on("peer-leave", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+});
