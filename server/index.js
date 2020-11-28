62bff7ac005e2f33eea6edc15111b581b20d12a6
qinben10 <qinben10@gmail.com>
2020-11-28 13:29:13 -0500

    minor change for local test

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(morgan('volleyball'));
 app.use(express.json());
 
-app.use(cookieParser())
-app.use(authentication)
+// app.use(cookieParser())
+// app.use(authentication)
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api', router)
@@ -27,15 +27,15 @@ app.use((err, req, res, next) => {
 
 
 
-// const init = async () => {
-// try {
-//     await db.sync({force: true});
-//     const port = process.env.PORT || 8080;
-//     app.listen(port, () => console.log(`listening on port ${port}`));
-// }
-// catch (ex){
-//     console.log(ex);
-// }
-// };
+const init = async () => {
+try {
+    // await db.sync({force: true});
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => console.log(`listening on port ${port}`));
+}
+catch (ex){
+    console.log(ex);
+}
+};
 
-// init();
+init();
