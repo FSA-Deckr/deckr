f939998498af92a5156bc1a2b93bdb2a17b933e5
qinben10 <qinben10@gmail.com>
2020-11-29 18:38:30 -0500

    minor change for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/deckr', {logging: false})
+const db = new Sequelize(process.env.DATABASE_URL || 
+    'postgres://lalhost/deckr'
+    // 'postgres://localhost/deckr'
+    , {logging: false})
 
 module.exports = db
