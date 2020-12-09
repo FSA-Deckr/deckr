e45d87f1d1d8d301f7173a37ef0302c1345d4274
John Cook <jpcook72@gmail.com>
2020-12-08 22:56:02 -0600

    Take out agora api stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 
 router.use('/game', require('./game'))
 router.use('/player', require('./player'))
-router.use('/agora', require('./agora'))
 
 router.use((req, res, next) => {
     const err = new Error('API route not found!')
