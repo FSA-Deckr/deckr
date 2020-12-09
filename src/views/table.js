4b3704a0afe103a2309a5c2a903adf186d593d43
John Cook <jpcook72@gmail.com>
2020-12-08 20:42:54 -0600

    ChannelNum update

--- src/views/table.js
+++ src/views/table.js
@@ -62,7 +62,7 @@ async function renderTable(tableNumber,playerNumber) {
       </body>`
 
         const game = new DeckrTable(socket, room, playerNumber)
-        startVideo(channelNum)
+        startVideo()
 }
 
 module.exports = {attemptToRenderTable, renderTable}
